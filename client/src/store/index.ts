fb098e743869f8f231960f47dea62578880f8296
Shamil <toshamilgis@gmail.com>
2022-05-28 20:34:46 +0300

    Configure React app for deployment to GitHub Pages

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,23 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { catsReducer } from './cats/catsReducer';
-import { persistReducer } from 'redux-persist';
-import { combineReducers } from '@reduxjs/toolkit';
-import storage from 'redux-persist/lib/storage';
+import { configureStore } from "@reduxjs/toolkit";
+import { catsReducer } from "./cats/catsReducer";
+import { persistReducer } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
 
 const persistConfig = {
-    key: 'cats',
-    storage,
+  key: "cats",
+  storage,
 };
 
 const reducers = combineReducers({ cats: catsReducer });
 const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = configureStore({
-    reducer: persistedReducer,
-    middleware: getDefaultMiddleware =>
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
-})
+});
 
 export default store;
